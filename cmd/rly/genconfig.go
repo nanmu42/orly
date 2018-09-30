42e69a1b07a6e9b7cbe1e61428b7c10cda63d515
nanmu42 <i@nanmu.me>
2018-09-30 15:37:49 +0800

    [dev] init rly

---
+++ cmd/rly/genconfig.go
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 LI Zhennan
+ *
+ * Use of this work is governed by an MIT License.
+ * You may find a license copy in project root.
+ *
+ */
+
+// +build genconfig
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}()
+
+	content, err := C.Info()
+	if err != nil {
+		err = errors.Wrap(err, "C.Info")
+		return
+	}
+	f, err := os.Create("config_example.toml")
+	if err != nil {
+		err = errors.Wrap(err, "os.Create")
+		return
+	}
+	defer f.Close()
+
+	content = `# Copy this file to create default config
+#
+# cp config_example.toml config.toml
+
+` + content
+
+	_, err = f.WriteString(content)
+	if err != nil {
+		err = errors.Wrap(err, "f.WriteString")
+		return
+	}
+
+	return
+}
---
+++ cmd/rly/main.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 LI Zhennan
+ *
+ * Use of this work is governed by an MIT License.
+ * You may find a license copy in project root.
+ *
+ */
+
+// rly is an API for O'RLY cover generation
+package main
+
+func main() {
+
+}
